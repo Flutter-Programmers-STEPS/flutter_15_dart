1c2db7fc98657a9cd41768808d5dc52bbd7aa3ce
Java_programmers-STEPS <sreelakshmis_2445@spectrum.net.in>
2023-06-27 11:23:05 +0530

    Done

---
+++ lib/27062023/testEmployee.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_15/27062023/employee.dart';
+
+main() {
+  var emp1=Employee();
+  emp1.id=101;
+  emp1.department="Technical";
+  emp1.empName="Sreelakshmi";
+
+  print(emp1.id);
+  print(emp1.department);
+  print(emp1.empName);
+ // print(emp1._id); Not possible becouse fields are private
+
+}
