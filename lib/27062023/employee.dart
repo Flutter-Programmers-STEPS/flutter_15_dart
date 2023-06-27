e6b9390666bd422b1ffb9f65897ded7af40dfe71
Java_programmers-STEPS <sreelakshmis_2445@spectrum.net.in>
2023-06-27 11:23:05 +0530

    Done

---
+++ lib/27062023/employee.dart
@@ -0,0 +1,27 @@
+class Employee{
+  int? _id;
+  String? _empName;
+  String? _department;
+// any variable or methods created with "_" is called private variable
+// Encapsulation will perform only
+// when the variables and functions are declared as private
+
+  int get id => _id!;
+
+  set id(int value) {
+    _id = value;
+  }
+
+  String get empName => _empName!;
+
+  String get department => _department!;
+
+  set department(String value) {
+    _department = value;
+  }
+
+  set empName(String value) {
+    _empName = value;
+  }
+
+}
